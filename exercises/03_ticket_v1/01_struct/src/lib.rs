ea3b98ddff6eed3a367e71f425586d641f4f8857
pystole <pystole@gmail.com>
2025-02-19 10:18:44 +0100

    03 complete

--- exercises/03_ticket_v1/01_struct/src/lib.rs
+++ exercises/03_ticket_v1/01_struct/src/lib.rs
@@ -5,6 +5,20 @@
 // It should also have a method named `is_available` that returns a `true` if the quantity is
 // greater than 0, otherwise `false`.
 
+struct Order {
+    price: u32,
+    quantity: u32,
+}
+
+impl Order {
+    fn is_available(self) -> bool {
+        self.quantity > 0
+    }
+    fn zero() -> Order {
+        Order { price: 0, quantity: 0 }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
