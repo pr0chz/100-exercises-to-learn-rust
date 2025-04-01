50b18faf8a29f3d3dc6e59da629336e839a41fc6
pystole <pystole@gmail.com>
2025-04-01 15:45:21 +0200

    Fixed error handling + added tests

--- exercises/08_futures/08_outro/src/main.rs
+++ exercises/08_futures/08_outro/src/main.rs
@@ -1,3 +1,4 @@
+use thiserror::Error;
 use outro_08::data;
 use outro_08::data::{Status, Ticket};
 use outro_08::store::{TicketId, TicketStore};
@@ -5,6 +6,8 @@ use ticket_fields::{TicketDescription, TicketTitle};
 use tide::prelude::*;
 use tide::{Body, Request, Response, StatusCode};
 use tokio::net::TcpListener;
+use MyError::NotFound;
+use crate::MyError::BadRequest;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(remote = "TicketTitle")]
@@ -62,9 +65,32 @@ async fn listen(port: Option<u16>) -> std::io::Result<TcpListener> {
     Ok(listener)
 }
 
+#[derive(Error, Debug)]
+pub enum MyError {
+    #[error("Bad request: {0}")]
+    BadRequest(String),
+
+    #[error("Not found: {0}")]
+    NotFound(String),
+}
+
+async fn error_handler(mut res: Response) -> tide::Result {
+    if let Some(error) = res.downcast_error::<MyError>() {
+        let status_code = match error {
+            BadRequest(_) => { StatusCode::BadRequest }
+            NotFound(_) => { StatusCode::NotFound }
+        };
+        let error_msg = error.to_string();
+        res.set_status(status_code);
+        res.set_body(error_msg);
+    }
+    Ok(res)
+}
+
 async fn run_server(listener: TcpListener) -> std::io::Result<()> {
     let store = TicketStore::new();
     let mut app = tide::with_state(store);
+    app.with(tide::utils::After(error_handler));
     app.at("/tickets").post(new_ticket);
     app.at("/tickets/:id").get(get_ticket);
     // Use the listener for the Tide app
@@ -72,10 +98,12 @@ async fn run_server(listener: TcpListener) -> std::io::Result<()> {
 }
 
 async fn new_ticket(mut req: Request<TicketStore>) -> tide::Result {
-    // TODO improve error handling
-    let ticket_request: CreateTicketRequest = req.body_json().await?;
+    let ticket_request: CreateTicketRequest = req.body_json()
+        .await.map_err(|_| BadRequest("Failed to parse.".to_string()))?;
+
     let store = req.state();
-    let id: TicketId = store.write().await.add_ticket(ticket_request.try_into()?);
+    let ticket_draft = ticket_request.try_into().map_err(|e| BadRequest(format!("Malformed ticket: {}", e)))?;
+    let id: TicketId = store.write().await.add_ticket(ticket_draft);
 
     let response_body = CreateTicketResponse { ticket_id: id };
 
@@ -85,10 +113,12 @@ async fn new_ticket(mut req: Request<TicketStore>) -> tide::Result {
 }
 
 async fn get_ticket(req: Request<TicketStore>) -> tide::Result {
-    // TODO improve error handling
-    let ticket_id: u64 = req.param("id")?.parse::<u64>()?;
+    let ticket_id = req
+        .param("id").map_err(|_| BadRequest("Missing id parameter".to_string()))?
+        .parse::<u64>().map_err(|_| BadRequest("Wrong id parameter".to_string()))?;
+
     let store = req.state();
-    let inner_lock = store.read().await.get(TicketId(ticket_id)).unwrap();
+    let inner_lock = store.read().await.get(TicketId(ticket_id)).ok_or(NotFound("Ticket not found".to_string()))?;
     let ticket_guard = inner_lock.read();
     let ticket = ticket_guard.await;
 
@@ -108,6 +138,7 @@ mod tests {
     use std::net::SocketAddr;
     use surf;
     use surf::Response;
+    use tide::StatusCode;
 
     fn create_ticket_request(n: u64) -> CreateTicketRequest {
         CreateTicketRequest {
@@ -128,7 +159,7 @@ mod tests {
 
 
     #[tokio::test]
-    async fn basic_server() {
+    async fn basic_server_functions() {
         let listener = listen(None).await.unwrap();
         let address = listener.local_addr().unwrap().clone();
         let server = tokio::spawn(run_server(listener));
@@ -177,4 +208,50 @@ mod tests {
 
         server.abort();
     }
+
+    #[tokio::test]
+    async fn malformed_new_ticket_request() {
+        let listener = listen(None).await.unwrap();
+        let address = listener.local_addr().unwrap().clone();
+        let server = tokio::spawn(run_server(listener));
+
+        let response = surf::post(format!("http://{}/tickets", address))
+            .body_string("not a json".to_string()).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::BadRequest);
+
+        server.abort();
+    }
+
+    #[tokio::test]
+    async fn malformed_get_request() {
+        let listener = listen(None).await.unwrap();
+        let address = listener.local_addr().unwrap().clone();
+        let server = tokio::spawn(run_server(listener));
+
+        let ticket_req = CreateTicketRequest {
+            title: "Really really long title, so long that it does not really fit in and it will fail validation and everything will blow up".to_string(),
+            description: "Description".to_string(),
+        };
+
+        let response = create_ticket(&address, &ticket_req).await;
+
+        assert_eq!(response.status(), StatusCode::BadRequest);
+
+        server.abort();
+    }
+
+    #[tokio::test]
+    async fn ticket_not_found() {
+        let listener = listen(None).await.unwrap();
+        let address = listener.local_addr().unwrap().clone();
+        let server = tokio::spawn(run_server(listener));
+
+        let response = get_ticket(&address, TicketId(333)).await;
+
+        assert_eq!(response.status(), StatusCode::NotFound);
+
+        server.abort();
+    }
+
 }
